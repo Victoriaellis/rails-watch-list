efc4ec38af23e5926263c030faae0eae14a66824
Robyn Ellison <robyn.victoria.ellison@gmail.com>
2021-05-27 15:59:38 +0100

    api created and full functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+Bookmark.destroy_all
+Movie.destroy_all
+
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+movie_serialized = URI.open(url).read
+movie = JSON.parse(movie_serialized)
+movie_list = movie["results"]
+# puts movie_list
+
+movie_list.each do |movie|
+  # puts movie["title"]
+  # puts movie["overview"]
+  # puts movie["poster_path"]
+  # puts movie["vote_average"]
+ Movie.create!(title: movie["title"], overview: movie["overview"], poster_url: movie["poster_path"], rating: movie["vote_average"])
+end
